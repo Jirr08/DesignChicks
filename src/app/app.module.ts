3862252ee1dab06d4d94b1202802ac6b65b24675
Jose <jirr081696@gmail.com>
2023-02-09 01:49:46 -0400

    Design almost finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { ItemsListComponent } from './components/items-list/items-list.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     SearchBarComponent,
-    ItemsListComponent
+    ItemsListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
